0430b66e8ec664a1aa1747548f5e71136fd7a88b
Andrew Phommathep <>
2023-11-01 09:46:53 -0700

    Increase concurrency and improve parallel processing with async/await and a fixed number of concurrent log source pop ops

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -4,20 +4,41 @@ const MinHeap = require("./min-heap");
 
 // Print all entries, across all of the *async* sources, in chronological order.
 
-module.exports = (logSources, printer) => {
-  return new Promise((resolve, reject) => {
-    const minHeap = new MinHeap();
+module.exports = async (logSources, printer) => {
+  const minHeap = new MinHeap();
+  const concurrencyLimit = 10; // Adjust the concurrency limit as needed.
 
-    // Initial population of the min heap
-    Promise.all(logSources.map((source) => source.popAsync())).then(logs => {
-      logs.forEach(log => minHeap.add({ entry: log, source }));
-    })
-      .catch(reject);
+  // Create an array to track active promises
+  const activePromises = [];
 
+  // Function to pop log entries from a source and add to the minHeap
+  async function popAndAddToHeap(source) {
+    while (true) {
+      const log = await source.popAsync();
+      if (log === false) {
+        return;
+      }
+      minHeap.add({ entry: log, source });
+    }
+  }
+
+  // Start pop operations for log sources
+  for (const source of logSources) {
+    for (let i = 0; i < concurrencyLimit; i++) {
+      const promise = popAndAddToHeap(source);
+      activePromises.push(promise);
+    }
+  }
+
+  try {
+    // Wait for all pop operations to complete
+    await Promise.all(activePromises);
+
+    // Function to process the next entry
     function processNextEntry() {
       if (minHeap.isEmpty()) {
         printer.done();
-        resolve(console.log("Async sort complete."));
+        console.log("Async sort complete.");
         return;
       }
 
@@ -27,18 +48,14 @@ module.exports = (logSources, printer) => {
       printer.print(log);
 
       // Fetch and insert the next log entry from the same source
-      source
-        .popAsync()
-        .then(log => {
-          if (log !== false) {
-            minHeap.add({ log, source });
-          }
-          processNextEntry();
-        })
-        .catch(reject);
+      popAndAddToHeap(source)
+        .then(processNextEntry)
+        .catch(console.error); // Handle errors as needed
     }
 
     // Start processing the log entries
     processNextEntry();
-  });
+  } catch (error) {
+    console.error("Error while popping log entries:", error);
+  }
 };
